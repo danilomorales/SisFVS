096377965af2e3cc1259110d3f97020b51f04cec
Danilo <arnoldmorales@upeu.edu.pe>
2018-07-04 14:22:59 -0500

    Revert "Prueba"
    This reverts commit 85782d9c073f5e83aeba073895702fff8826878c.

--- SistemaFigueri/CapaDatos/CDUsuario.cs
+++ SistemaFigueri/CapaDatos/CDUsuario.cs
@@ -15,9 +15,11 @@ namespace CapaDatos
 
         public SqlDataReader iniciarSesion(string usuario, string contrasena)
         {
-            
+            string sql = "select * from Usuario where Login='"+usuario+"' and Pass='"+contrasena+"'";
             SqlCommand comando = new SqlCommand();
-       
+            comando.Connection = Conexion.AbrirConexion();
+            comando.CommandText = sql;
+            leer = comando.ExecuteReader();
     
             return leer;
         }
