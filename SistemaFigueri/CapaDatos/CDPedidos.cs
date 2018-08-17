805a298c4eb806a1cafd3cda44040fe924210e96
AlphaLeader <AlphaLeader@UPEU>
2018-08-16 20:46:30 -0500

    ahhhhh

--- SistemaFigueri/CapaDatos/CDPedidos.cs
+++ SistemaFigueri/CapaDatos/CDPedidos.cs
@@ -19,10 +19,10 @@ namespace CapaDatos
                 "cli.RUC, tidoc.Descripcion,cli.NroDocumento, ti.NombreTienda,concat(res.Nombres,' ',res.Apellidos) as Responsable,pedido.FechaPedido,pedido.Valor,pedido.IGV,pedido.MontoTotal," +
                 "pedido.Descuento,pedido.TotalEnviado " +
                 "from PEDIDO_PRODUCTO pedido " +
-                "LEFT JOIN  Caja.CLIENTE cli ON pedido.IdCliente = cli.IdCliente " +
-                "LEFT JOIN dbo.TIPO_DOC_IDENT tidoc ON cli.IdTipoDocIdent=tidoc.IdTipoDocIdent " +
-                "LEFT JOIN dbo.TIENDA ti ON pedido.IdTienda = ti.IdTienda " +
-                "LEFT JOIN dbo.RESPONSABLE res ON pedido.IdResponsable = res.IdResponsable " +
+                "INNER JOIN  Caja.CLIENTE cli ON pedido.IdCliente = cli.IdCliente " +
+                "INNER JOIN dbo.TIPO_DOC_IDENT tidoc ON cli.IdTipoDocIdent=tidoc.IdTipoDocIdent " +
+                "INNER JOIN dbo.TIENDA ti ON pedido.IdTienda = ti.IdTienda " +
+                "INNER JOIN dbo.RESPONSABLE res ON pedido.IdResponsable = res.IdResponsable " +
                 "WHERE pedido.FechaPedido >= TRY_PARSE(@FechaPedido1 as datetime using 'es-ES') " +
                 "and pedido.FechaPedido <= TRY_PARSE(@FechaPedido2 as datetime using 'es-ES') " +
                 "and pedido.IdPedidoProducto NOT IN(select DISTINCT IdPedidoProducto from dbo.DETALLE_PEDIDO_PRODUCTO where Devolucion='0') " +
@@ -41,6 +41,27 @@ namespace CapaDatos
                 Console.WriteLine("Error al buscar producto: "+ex);
             }
             return adapter;
+        }
+
+        public SqlDataAdapter buscarDetallePedido(String idPedido)  
+        {
+                String sql = "SELECT det.IdProducto,pro.DescripcionProducto,det.Precio,pro.Stock,det.Cantidad FROM dbo.DETALLE_PEDIDO_PRODUCTO det " +
+                        ", dbo.PEDIDO_PRODUCTO ped, dbo.PRODUCTO pro " +
+                        "WHERE ped.IdPedidoProducto = det.IdPedidoProducto " +
+                        "AND det.IdProducto = pro.IdProducto " +
+                        "AND ped.IdPedidoProducto = @IdPedido; ";
+                SqlConnection SqlCon = new SqlConnection();
+                try
+                {
+                    adapter = new SqlDataAdapter(sql, Conexion.AbrirConexion());
+                    adapter.SelectCommand.Parameters.AddWithValue("@IdPedido", idPedido);
+ 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al buscar detalle: " + ex);
+                }
+                return adapter;
         }
     }
 }
