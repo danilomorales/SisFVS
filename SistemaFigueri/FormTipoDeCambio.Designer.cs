df5e2ee590f91190ea081606892551b71554c2a9
Arnold <arnoldmorales@upeu.edu.pe>
2018-08-26 18:51:42 -0500

    ad

--- SistemaFigueri/FormTipoDeCambio.Designer.cs
+++ SistemaFigueri/FormTipoDeCambio.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "FormTipoDeCambio";
             this.Text = "FormTipoDeCambio";
+            this.Load += new System.EventHandler(this.FormTipoDeCambio_Load);
             this.ResumeLayout(false);
 
         }
